f3379a223755c6b49d716ceef03f747c131aa088
rafa-script <rafaeldscarvalho@hotmail.com>
2026-03-13 17:06:42 -0300

    feat: add toolbar buttons, implement some button actions, and include icons folder

---
+++ src/main/java/com/mycompany/compilador/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.compilador;
+
+/**
+ *
+ * @author 320255337
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
